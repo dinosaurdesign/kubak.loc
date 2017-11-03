adbb122bf25fc4191e7a57d51d31ee3e9b959b50
dinosaur design <info@dds.by>
2017-11-03 11:20:07 +0300

    path

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+//переменные модулей
+var gulp = require ('gulp'),
+    sass = require ('gulp-sass'),
+    browsersynch = require ('browser-sync'),
+    reload = browserSync.reload,
+    watch = require ('gulp-watch');
+//переменные путей
+var path = {
+    src: {
+
+},
+    dist: {
+
+    },
+};
+
+
