2d52d0a92c2709c7ff2d9ad9e8c2e9b7ca942392
dinosaur design <info@dds.by>
2017-11-03 19:09:45 +0300

    boiler

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
     concat = require('gulp-concat'),
-    browsersynch = require('browser-sync'),
+    browsersync = require('browser-sync'),
     watch = require('gulp-watch');
 //переменные путей
 var path = {
     src: {
+        html: 'src/*.html',
         css: 'src/css/',
         sass: 'src/sass/**/*.*',
         js: 'src/js/**/*.*'
@@ -20,12 +21,24 @@ var path = {
     }
 };
 // tasks
-gulp.task('sass', function () {
+gulp.task('sass', function () {// компиляция sass
     return gulp.src(path.src.sass) // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(sourcemaps.init())
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true})) // Создаем префиксы
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.src.css)) // Выгружаем результата в папку
+});
+gulp.task('browsersync', function () {
+    browsersync({
+        server: {
+            baseDir: "src"
+        },
+        notify: false
+    });
+});
+gulp.task('watch', ['browsersync', 'sass'], function () {
+    gulp.watch(path.src.css, ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch(path.src.html, browsersync.reload); // Наблюдение за HTML файлами в корне проекта
 });
 
